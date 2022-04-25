f9d93395b1419525bf5bf31941c07884493263a0
behzad.mousavi@snapp.cab <behzad.mousavi@snapp.cab>
2022-04-25 23:53:11 +0430

    Add new subcommand to sum up even numbers

--- cmd/even.go
+++ cmd/even.go
@@ -25,13 +25,8 @@ import (
 // evenCmd represents the even command
 var evenCmd = &cobra.Command{
 	Use:   "even",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Sum up even numbers",
+	Long:  `This is going to sum up even numbers among inputs`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("even called")
 		var evenSum int
